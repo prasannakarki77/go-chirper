acd59a095bdbdad2409c1c1fc6ea3624fadf3e8c
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-08-17 23:02:59 +0545

    healthz route

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ func main() {
 	fs := http.FileServer(http.Dir("."))
 	mux.Handle("/", fs)
 
+	mux.HandleFunc("/healthz", func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "text/plain; chartset=utf-8")
+		res.Header().Set("X-Hello-Bootdev", "Showing these being set")
+		res.WriteHeader(200)
+		res.Write([]byte("OK"))
+	})
+
 	serv := &http.Server{Handler: mux, Addr: ":8080"}
 	serv.ListenAndServe()
 
