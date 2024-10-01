119a0e74b55f6ea63390bd1da30a9011a1781a68
prasannakarki77 <prasanna.karki77@gmail.com>
2024-10-01 22:37:44 +0545

    validate jwt auth function

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -42,3 +42,29 @@ func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (str
 
 	return signedToken, nil
 }
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if !token.Valid {
+		return uuid.Nil, jwt.ErrSignatureInvalid
+	}
+
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if !ok {
+		return uuid.Nil, jwt.ErrTokenInvalidClaims
+	}
+
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userID, nil
+}
